b98fd7b4e589509cdc145f93c8d6f64c96cf27fb
alecnguyen <apndizzle@gmail.com>
2016-07-14 09:41:33 -0700

    Project 1

---
+++ Projects/Project 1/Project1_BlackJackSimulator/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Alec Nguyen
+ * Created on July 14 2016, 9:39 AM
+ * Purpose: Black Jack Simulator
+ */
+
+//system libraries
+#include <iostream>//Input/Output Library
+#include <cstdlib>
+using namespace std;//Namespace of the System Libraries
+
+//User Libraries (Libraries created by the user)
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    
+    //Output the processed Data
+    
+    //Exit Stage Stage!
+    return 0;
+}
+
